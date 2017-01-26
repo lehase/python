f15110e14178e72345c285fc3a5d7edf25e40346
s_pyrkov <sdfsdfdsf>
2017-01-26 12:36:47 +0300

    98

---
+++ Date_mod.py
@@ -0,0 +1,14 @@
+import os
+import datetime
+import shutil
+
+dnow = datetime.datetime.now()
+for root, subFolders, files in os.walk('/edi'):
+    for file in files:
+        dmodify = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(root, file)))
+        days_diff = (dnow-dmodify).days
+        if days_diff > 270:
+            directory = str(os.path.join('/tmp/1'+root[4:]))
+            if not os.path.exists(directory):
+                    os.makedirs(directory)
+            shutil.copy(os.path.join(root, file), str(os.path.join('/tmp/1'+root[4:])+str(file)))
