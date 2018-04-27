9a26e85882b958170d45081767cc1a452d29859e
a_seregin <a@aa.com>
2018-04-27 11:22:32 +0300

    Added Flask application with forms

---
+++ microblog/app/routes.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'miguel'}
+    posts = [
+        {
+            'author':{'username':'Jo'},
+            'body':'Post1'
+        },
+        {
+            'author':{'username':'Gof'},
+            'body':'Postwww3'  
+        },
+        {
+            'author':{'username':'DDwwf'},
+            'body':'ADDAww3'  
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
