7792a3295f9730ad5ff098341c1b33809c07b66a
a_seregin <a@aa.com>
2018-04-27 14:05:02 +0300

    add model for users and db Migrations

--- microblog/config.py
+++ microblog/config.py
@@ -3,3 +3,11 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
+    #Database config
+    db_host = 'mow03-rdps13'
+    db_port = '1433'
+    db_name = 'flask'
+    db_user = 'flask'
+    db_pass = 'flask'
+    
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://{}:{}@{}:{}/{}?driver=SQL+Server+Native+Client+11.0'.format(db_user, db_pass, db_host, db_port, db_name)
